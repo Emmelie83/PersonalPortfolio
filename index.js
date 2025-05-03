57b08313a2db2b9797fe58f2422b153537982a63
Emmelie83 <ej224sk@student.lnu.se>
2025-05-03 21:50:46 +0200

    Add skills section

---
+++ index.js
@@ -0,0 +1,13 @@
+function toggleDescription() {
+    const toggleButtons = document.querySelectorAll(".more-text");
+
+    toggleButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const description = button.nextElementSibling;
+            description.classList.toggle("hidden");
+            button.textContent = description.classList.contains("hidden") ? "+ More" : "- Less";
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", toggleDescription);
